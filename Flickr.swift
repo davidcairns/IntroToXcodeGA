9c5776ab8554570592fdc606b35d2264a11e05e3
David Cairns <drcairns@gmail.com>
2016-05-21 16:43:09 -0400

    Updates Flickr.swift to grab my secret key file if it is available

--- Flickr.swift
+++ Flickr.swift
@@ -1,63 +1,82 @@
 import Foundation
-import AppKit
+import UIKit
 
 public class Flickr {
-	let API_KEY = ""
+	let apiKey: String
 	
-	public init() { }
+	public init(apiKey: String) {
+		self.apiKey = apiKey
+	}
+	convenience public init() {
+		let url = NSURL(string: "http://davidcairns.org/src/flickr-api-key.txt")!
+		let data = NSData(contentsOfURL: url)!
+		let key = NSString(data: data, encoding: NSUTF8StringEncoding)!
+		self.init(apiKey: key.substringToIndex(key.length - 1))
+	}
 	
 	private func flickrSearchURLForSearchTerm(searchTerm:String) -> NSURL {
-//		let escapedTerm = searchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
-		
-		let escapedTerm = searchTerm.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet())
-		let URLString = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&text=\(escapedTerm)&per_page=30&format=json&nojsoncallback=1"
+		let escapedTerm = searchTerm.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())!
+		let URLString = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(self.apiKey)&text=\(escapedTerm)&per_page=30&format=json&nojsoncallback=1"
 		return NSURL(string: URLString)!
 	}
 	
 	public func photoURLsForSearch(search: String) -> [NSURL] {
 		let url = flickrSearchURLForSearchTerm(search)
-//		guard let data = NSData(contentsOfURL: url) else {
-//			print("ERROR: Falied to request data from \(url)")
-//			return []
-//		}
-//		let resultsDictionary = try! NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions(rawValue: 0)) as? NSDictionary
+		guard let data = NSData(contentsOfURL: url) else {
+			print("ERROR: Falied to request data from \(url)")
+			return []
+		}
+		let resultsDictionary = try! NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions(rawValue: 0)) as? NSDictionary
 		
-//		switch (resultsDictionary!["stat"] as! String) {
-//		case "ok":
-//			break
-//		default:
-//			return []
-//		}
-//		
-//		let photosContainer = resultsDictionary!["photos"] as! NSDictionary
-//		let photosReceived = photosContainer["photo"] as! [NSDictionary]
-//		
-//		return photosReceived.map {
-//			photoDictionary in
-//			
-//			let photoID = photoDictionary["id"] as? String ?? ""
-//			let farm = photoDictionary["farm"] as? Int ?? 0
-//			let server = photoDictionary["server"] as? String ?? ""
-//			let secret = photoDictionary["secret"] as? String ?? ""
-//			
-//			return NSURL(string: "http://farm\(farm).staticflickr.com/\(server)/\(photoID)_\(secret)_m.jpg")!
-//		}
-		return []
+		let resultStatus = resultsDictionary!["stat"] as! String
+		switch (resultStatus) {
+		case "ok":
+			break
+		default:
+			print("ERROR: request returned status '\(resultStatus)'")
+			return []
+		}
+		
+		let photosContainer = resultsDictionary!["photos"] as! NSDictionary
+		let photosReceived = photosContainer["photo"] as! [NSDictionary]
+		
+		return photosReceived.map {
+			photoDictionary in
+			
+			let photoID = photoDictionary["id"] as? String ?? ""
+			let farm = photoDictionary["farm"] as? Int ?? 0
+			let server = photoDictionary["server"] as? String ?? ""
+			let secret = photoDictionary["secret"] as? String ?? ""
+			
+			return NSURL(string: "http://farm\(farm).staticflickr.com/\(server)/\(photoID)_\(secret)_m.jpg")!
+		}
+	}
+	
+	
+	// Loading images
+	public func imageFromURL(string: String) -> UIImage {
+		return imageFromURL(NSURL(string: string)!)
+	}
+	public func imageFromURL(URL: NSURL) -> UIImage {
+		let response = NSData(contentsOfURL: URL)!
+		return UIImage(data: response)!
+	}
+	
+	
+	// ASYNC
+	private func doAsync <T> (block: () -> T, then: T -> ()) {
+		let queue = dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)
+		dispatch_async(queue) {
+			let result = block()
+			dispatch_async(dispatch_get_main_queue()) {
+				then(result)
+			}
+		}
 	}
 	public func photoURLsForSearchAsync(search: String, completion: [NSURL] -> ()) {
 		doAsync({ self.photoURLsForSearch(search) } , then: completion)
 	}
-	
-	
-	
-	public func imageFromURL(string: String) -> NSImage {
-		return imageFromURL(NSURL(string: string)!)
-	}
-	public func imageFromURL(URL: NSURL) -> NSImage {
-		let response = NSData(contentsOfURL: URL)!
-		return NSImage(data: response)!
-	}
-	public func imageFromURLAsync(URL: NSURL, completion: NSImage? -> ()) {
+	public func imageFromURLAsync(URL: NSURL, completion: UIImage? -> ()) {
 		doAsync({ self.imageFromURL(URL) }, then: completion)
 	}
 }
